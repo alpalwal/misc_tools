34dcb345fdb42d3ec3cd5de1d3c7eeff4db60cb6
alpalwal <alex@abcwebdesigner.com>
2017-11-08 17:04:41 -0800

    added python script for getting instance count

---
+++ get_aws_instance_count.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#python script for getting a list of all instances in ALL regions in an AWS account. 
+#This runs on the default CLI profile by default. If you want to change that, run 'export AWS_PROFILE=<yourProfileName>' before executing the script
+#Taken from here: https://stackoverflow.com/questions/40164786/determine-how-many-aws-instances-are-in-a-zone
+
+import boto.ec2
+
+for region in [r for r in boto.ec2.regions() if r.name not in ['cn-north-1', 'us-gov-west-1']]:
+  conn = boto.ec2.connect_to_region(region.name)
+  reservations = conn.get_all_instances()
+  for r in reservations:
+    for i in r.instances:
+      print region.name, i.id, i.tags, i.state
